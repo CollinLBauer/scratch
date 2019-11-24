fc59892114aa4eac02efdad876c4ac0e6511c7b3
Collin <bauercl@g.cofc.edu>
2019-11-24 14:45:58 -0500

    sync laptop changes

--- python/runescape/utilities/utilities.py
+++ python/runescape/utilities/utilities.py
@@ -1,8 +1,13 @@
 import datetime
 
 def get_time():
-    date = datetime.date.today()
-    time = datetime.datetime.now().strftime("%H:%M:%S")
-
-    return "{}: {}".format(date,time)
-
+    dateList = str(datetime.date.today()).split("-")
+    dateList = [dateList[1],dateList[2],dateList[0]]
+    date = "/".join(dateList)
+    hours = int(datetime.datetime.now().strftime("%H"))
+    minutes = datetime.datetime.now().strftime("%M:%S")
+    if int(hours) > 12:
+        ampm = "PM"
+    else:
+        ampm = "AM"
+    return "{} {}:{} {}".format(date,hours%12,minutes,ampm)
